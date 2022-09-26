22db553e01ccdc80afe0affe87daaf813a9d3ec8
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-09-26 14:10:36 +0300

    urls

--- devs/views.py
+++ devs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def developer(request):
+    context={}
+    return HttpResponse('i code everyday')
+def index(request):
+    return render(request, 'index.html')    
